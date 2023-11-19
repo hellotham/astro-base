94b971ceea3544e6c742fbc686eb9527e0867ec7
Christine Tham <chris.tham@hellotham.com>
2023-11-19 13:00:02 +1100

    Update to latest packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true
-  },
   site: 'https://astro-base.netlify.app',
   integrations: [
     UnoCSS({
