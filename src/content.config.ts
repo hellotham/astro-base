d6ebc244d5dfa9c7abf6e6d8a0272bd1633d40f6
Christine Tham <chris.tham@hellotham.com>
2024-12-10 08:41:19 +1100

    Upgrade to Astro 5

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
 import { defineCollection } from 'astro:content'
 import { rssSchema } from '@astrojs/rss'
+import { glob } from 'astro/loaders'
 
 const blog = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/blog' }),
   schema: rssSchema
 })
 
